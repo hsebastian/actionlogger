24754e9965fdb34ac1f8b2674905bab248291722
Hans <hnsbstn@gmail.com>
2009-10-08 10:06:58 -0700

    updated packaging.py

--- packaging/packaging.py
+++ packaging/packaging.py
@@ -20,6 +20,8 @@ os.mkdir(newdir)
 os.rename(os.path.join(os.curdir, jar), os.path.join(newdir, jar))
 
 packagename = "actionlogger.xpi"
+if os.path.isfile(os.path.join(os.curdir, packagename)):
+  os.remove(os.path.join(os.curdir, packagename))
 package = ZipFile(os.path.join(os.curdir, packagename), 'w')
 package.write(os.path.join(newdir, jar))
 package.write(os.path.join(os.curdir, "chrome.manifest"))
@@ -27,6 +29,10 @@ package.write(os.path.join(os.pardir, "install.rdf"))
 package.write(os.path.join(os.pardir, "readme.txt"))
 package.close()
 
+result = ZipFile(os.path.join(os.curdir, packagename), 'r')
+if result.testzip() is None:
+  print ".xpi is created successfully"
+  
 # clean up
 os.remove(os.path.join(newdir, jar))
 os.removedirs(os.path.join(os.curdir, "chrome"))
