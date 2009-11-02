0d9ed1ee3976fb0e97b77df3071f5e8edc793bea
Hans <hnsbstn@gmail.com>
2009-11-02 11:30:58 -0800

    updated overlay code to remove unnecessary code

--- content/overlay.js
+++ content/overlay.js
@@ -1,12 +1,5 @@
 var ActionLoggerOverlay = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-  },
-
   onMenuItemCommand: function() {
     window.open("chrome://actionlogger/content/main-window.xul", "", "chrome");
   }
 };
-
-window.addEventListener("load", function(e) { ActionLoggerOverlay.onLoad(e); }, false); 
